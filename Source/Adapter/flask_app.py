46ec344ea68fd1bfc5c4674ba9678fc55eded59d
Garrett Hansen <kirypto@gmail.com>
2020-10-12 12:10:06 -0600

    Modify flask app to statically serve api specifications

--- Source/Adapter/flask_app.py
+++ Source/Adapter/flask_app.py
@@ -1,9 +1,27 @@
+from pathlib import Path
+
 from flask import Flask
 
 
-app = Flask(__name__)
+# File Paths
+_PROJECT_ROOT = Path(__file__).parents[2]
+_RESOURCE_FOLDER = _PROJECT_ROOT.joinpath("Source", "Resources")
 
 
-@app.route('/')
+# Construct Flask web service
+flask_web_app = Flask(
+    __name__,
+    root_path=_RESOURCE_FOLDER.joinpath("FlaskWebAppRoot").as_posix(),
+    static_folder=_RESOURCE_FOLDER.joinpath("StaticallyServedFiles").as_posix(),
+    static_url_path="/static",
+)
+
+
+# Setup web path root
+@flask_web_app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'It Works!'
+
+
+if __name__ == '__main__':
+    flask_web_app.run()
