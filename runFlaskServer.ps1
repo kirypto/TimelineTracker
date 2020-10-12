c8542b1b3716674dab2d23ddbe4c1b94a01e0762
Garrett Hansen <kirypto@gmail.com>
2020-10-12 15:15:24 -0600

    Change adapter package to lowercase

--- runFlaskServer.ps1
+++ runFlaskServer.ps1
@@ -1,4 +1,4 @@
 # Runs the Timeline Tracker REST API via Flask microservice
 
-$env:FLASK_APP = './Source/Python/Adapter/flask_app.py';
+$env:FLASK_APP = './Source/Python/adapter/flask_app.py';
 ./venv/Scripts/python -m flask run;
