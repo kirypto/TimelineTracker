5d72101c3836d5e315737af0eafbf5dd01d0ff50
Garrett Hansen <kirypto@gmail.com>
2020-10-15 21:56:38 -0600

    Implement NamedEntity

--- Source/Python/domain/descriptors.py
+++ Source/Python/domain/descriptors.py
@@ -1,5 +1,18 @@
+from re import match
+
+
 class NamedEntity:
-    pass
+    _name: str
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    def __init__(self, *, name: str = "", **kwargs):
+        if not isinstance(name, str) or not match(r"^[\w\- ]*$", name):
+            raise ValueError("name must be a string with only alphanumeric, underscore, dash, and space characters")
+        self._name = name
+        super().__init__(**kwargs)
 
 
 class DescribedEntity:
