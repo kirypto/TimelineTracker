6a06e631043175b29d8499ae4abec2ad358ed487
Garrett Hansen <kirypto@gmail.com>
2020-10-18 15:23:14 -0600

    Implement Position class

--- Source/Python/domain/positions.py
+++ Source/Python/domain/positions.py
@@ -1,5 +1,37 @@
 class Position:
-    pass
+    _latitude: float
+    _longitude: float
+    _altitude: float
+    _continuum: float
+    _reality: int
+
+    @property
+    def latitude(self) -> float:
+        return self._latitude
+
+    @property
+    def longitude(self) -> float:
+        return self._longitude
+
+    @property
+    def altitude(self) -> float:
+        return self._altitude
+
+    @property
+    def continuum(self) -> float:
+        return self._continuum
+
+    @property
+    def reality(self) -> int:
+        return self._reality
+
+    def __init__(self, *, latitude: float, longitude: float, altitude: float, continuum: float, reality: int, **kwargs) -> None:
+        self._reality = reality
+        self._continuum = continuum
+        self._altitude = altitude
+        self._longitude = longitude
+        self._latitude = latitude
+        super().__init__(**kwargs)
 
 
 class PositionalRange:
