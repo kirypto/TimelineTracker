6e0d17c27c73647c54a56cce6de651e8aeed401e
Garrett Hansen <kirypto@gmail.com>
2020-10-21 18:56:17 -0600

    Cleanup flask_app.py

--- Source/Python/adapter/flask/flask_app.py
+++ Source/Python/adapter/flask/flask_app.py
@@ -7,40 +7,47 @@ from adapter.flask.flask_controller_locations import register_locations_routes
 from adapter.main import TimelineTrackerApp
 
 
-# File Paths
-_PROJECT_ROOT = Path(__file__).parents[4]
-_RESOURCE_FOLDER = _PROJECT_ROOT.joinpath("Source", "Resources")
+def _create_flask_web_app() -> Flask:
+    # File Paths
+    _PROJECT_ROOT = Path(__file__).parents[4]
+    _RESOURCE_FOLDER = _PROJECT_ROOT.joinpath("Source", "Resources")
 
-# Web Paths
-_SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
-_API_SPECIFICATION_URL = '/static/apiSpecification.json'  # Our API url (can of course be a local resource)
+    # Web Paths
+    _SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+    _API_SPECIFICATION_URL = '/static/apiSpecification.json'  # Our API url (can of course be a local resource)
 
-# Construct Flask web service
-flask_web_app = Flask(
-    __name__,
-    root_path=_RESOURCE_FOLDER.joinpath("FlaskWebAppRoot").as_posix(),
-    static_folder=_RESOURCE_FOLDER.joinpath("StaticallyServedFiles").as_posix(),
-    static_url_path="/static",
-)
+    # Construct Flask web service
+    flask_web_app = Flask(
+        __name__,
+        root_path=_RESOURCE_FOLDER.joinpath("FlaskWebAppRoot").as_posix(),
+        static_folder=_RESOURCE_FOLDER.joinpath("StaticallyServedFiles").as_posix(),
+        static_url_path="/static",
+    )
+
+    # Call flask_swagger_ui blueprint factory function
+    swagger_ui_blueprint = get_swaggerui_blueprint(
+        _SWAGGER_URL,
+        _API_SPECIFICATION_URL,
+    )
+
+    # Register blueprint at URL
+    # (URL must match the one given to factory function above)
+    flask_web_app.register_blueprint(swagger_ui_blueprint, url_prefix=_SWAGGER_URL)
+
+    # Setup web path root
+    @flask_web_app.route('/')
+    def hello_world():
+        return 'It Works!'
+
+    return flask_web_app
 
 
-# Setup web path root
-@flask_web_app.route('/')
-def hello_world():
-    return 'It Works!'
-
-
-# Call flask_swagger_ui blueprint factory function
-swagger_ui_blueprint = get_swaggerui_blueprint(
-    _SWAGGER_URL,
-    _API_SPECIFICATION_URL,
-)
-
-# Register blueprint at URL
-# (URL must match the one given to factory function above)
-flask_web_app.register_blueprint(swagger_ui_blueprint, url_prefix=_SWAGGER_URL)
-
-if __name__ == '__main__':
+def _main():
+    flask_web_app = _create_flask_web_app()
     timeline_tracker_application = TimelineTrackerApp()
     register_locations_routes(flask_web_app, timeline_tracker_application)
     flask_web_app.run()
+
+
+if __name__ == '__main__':
+    _main()
