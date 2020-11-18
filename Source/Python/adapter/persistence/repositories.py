c3a2e417adc22356ffe96d8752ccb43402d5f349
Garrett Hansen <kirypto@gmail.com>
2020-11-17 22:30:59 -0600

    Implement JsonFileLocationRepository's save method

--- Source/Python/adapter/persistence/repositories.py
+++ Source/Python/adapter/persistence/repositories.py
@@ -1,7 +1,9 @@
 from copy import deepcopy
+from json import dumps
 from pathlib import Path
 from typing import Set, Dict
 
+from adapter.request_handling.views import LocationView
 from domain.ids import PrefixedUUID
 from domain.locations import Location
 from domain.persistence.repositories import LocationRepository
@@ -55,7 +57,15 @@ class JsonFileLocationRepository(LocationRepository):
         self._location_repo_path = location_repo_path
 
     def save(self, location: Location) -> None:
-        raise NotImplementedError("Not yet implemented")
+        if not isinstance(location, Location):
+            raise TypeError(f"Argument 'location' must be of type {Location.__name__}")
+
+        location_file = self._location_repo_path.joinpath(f"{location.id}")
+        if location_file.exists() and not location_file.is_file():
+            raise FileExistsError(f"Could not save location {location.id}, an uncontrolled file already existed with the same name and path.")
+
+        json = LocationView.to_json(location)
+        location_file.write_text(dumps(json, indent=4), "utf8")
 
     def retrieve(self, location_id: PrefixedUUID) -> Location:
         raise NotImplementedError("Not yet implemented")
