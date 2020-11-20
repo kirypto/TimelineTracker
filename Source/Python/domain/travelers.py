b76809a260dcc53341522d8c9f36ce8cc3e4cfb4
Garrett Hansen <kirypto@gmail.com>
2020-11-19 20:54:02 -0600

    Implement Traveler class

--- Source/Python/domain/travelers.py
+++ Source/Python/domain/travelers.py
@@ -1,3 +1,13 @@
-class Traveler:
+from domain.descriptors import NamedEntity, DescribedEntity
+from domain.ids import IdentifiedEntity, PrefixedUUID
+from domain.positions import JourneyingEntity
+from domain.tags import TaggedEntity
+
+
+class Traveler(IdentifiedEntity, NamedEntity, DescribedEntity, JourneyingEntity, TaggedEntity):
     def __init__(self, **kwargs):
-        pass
+        if "id" in kwargs and isinstance(kwargs["id"], PrefixedUUID):
+            id: PrefixedUUID = kwargs["id"]
+            if not id.prefix == "traveler":
+                raise ValueError("id must begin with 'traveler'")
+        super().__init__(**kwargs)
