fe6d9a0400c6e88930a2d3f3e45a936f68580925
Garrett Hansen <kirypto@gmail.com>
2020-11-19 22:14:08 -0600

    Implement InMemoryTravelerRepository

--- Source/Python/adapter/persistence/in_memory_repositories.py
+++ Source/Python/adapter/persistence/in_memory_repositories.py
@@ -4,6 +4,7 @@ from typing import Set, Dict
 from domain.ids import PrefixedUUID
 from domain.locations import Location
 from domain.persistence.repositories import LocationRepository, TravelerRepository
+from domain.travelers import Traveler
 
 
 class InMemoryLocationRepository(LocationRepository):
@@ -40,4 +41,33 @@ class InMemoryLocationRepository(LocationRepository):
 
 
 class InMemoryTravelerRepository(TravelerRepository):
-    pass
+    _travelers_by_id: Dict[PrefixedUUID, Traveler]
+
+    def __init__(self) -> None:
+        self._travelers_by_id = {}
+
+    def save(self, traveler: Traveler) -> None:
+        if not isinstance(traveler, Traveler):
+            raise TypeError(f"Argument 'traveler' must be of type {Traveler}")
+
+        self._travelers_by_id[traveler.id] = traveler
+
+    def retrieve(self, traveler_id: PrefixedUUID) -> Traveler:
+        if not isinstance(traveler_id, PrefixedUUID):
+            raise TypeError(f"Argument 'traveler_id' must be of type {PrefixedUUID}")
+        if traveler_id not in self._travelers_by_id:
+            raise NameError(f"No stored traveler with id '{traveler_id}'")
+
+        return deepcopy(self._travelers_by_id[traveler_id])
+
+    def retrieve_all(self) -> Set[Traveler]:
+        return {
+            deepcopy(traveler)
+            for traveler in self._travelers_by_id.values()
+        }
+
+    def delete(self, traveler_id: PrefixedUUID) -> None:
+        if traveler_id not in self._travelers_by_id:
+            raise NameError(f"No stored traveler with id '{traveler_id}'")
+
+        self._travelers_by_id.pop(traveler_id)
