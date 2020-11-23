f01f08091f1100dbc26cf351203f44baca33b8a9
Garrett Hansen <kirypto@gmail.com>
2020-11-23 12:59:43 -0600

    Tie in TravelersRequestHandler with main TimelineTrackerApp

--- Source/Python/adapter/main.py
+++ Source/Python/adapter/main.py
@@ -1,12 +1,18 @@
-from adapter.request_handling.handlers import LocationsRequestHandler
+from adapter.request_handling.handlers import LocationsRequestHandler, TravelersRequestHandler
 
 
 class TimelineTrackerApp:
     _locations_request_handler: LocationsRequestHandler
+    _travelers_request_handler: TravelersRequestHandler
 
     @property
     def locations_request_handler(self) -> LocationsRequestHandler:
         return self._locations_request_handler
 
+    @property
+    def travelers_request_handler(self) -> TravelersRequestHandler:
+        return self._travelers_request_handler
+
     def __init__(self) -> None:
+        self._travelers_request_handler = TravelersRequestHandler()
         self._locations_request_handler = LocationsRequestHandler()
