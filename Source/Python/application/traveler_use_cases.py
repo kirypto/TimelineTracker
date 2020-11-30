ad072cb901b97397bab547f1f9b7145b9802f407
Garrett Hansen <kirypto@gmail.com>
2020-11-30 12:25:03 -0600

    Implement TravelerUseCase.create functionality

--- Source/Python/application/traveler_use_cases.py
+++ Source/Python/application/traveler_use_cases.py
@@ -1,2 +1,19 @@
+from uuid import uuid4
+
+from domain.ids import PrefixedUUID
+from domain.persistence.repositories import TravelerRepository
+from domain.travelers import Traveler
+
+
 class TravelerUseCase:
-    pass
+    _traveler_repository: TravelerRepository
+
+    def __init__(self, traveler_repository: TravelerRepository) -> None:
+        self._traveler_repository = traveler_repository
+
+    def create(self, **kwargs) -> Traveler:
+        kwargs["id"] = PrefixedUUID(prefix="traveler", uuid=uuid4())
+
+        traveler = Traveler(**kwargs)
+        return traveler
+
