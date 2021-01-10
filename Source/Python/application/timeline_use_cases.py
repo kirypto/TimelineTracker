2a3f83868493f40edcee17fae693cc44bede33b0
Garrett Hansen <kirypto@gmail.com>
2021-01-10 13:34:33 -0600

    Implement functionality to construct a Traveler's timeline in TimelineUseCase class

--- Source/Python/application/timeline_use_cases.py
+++ Source/Python/application/timeline_use_cases.py
@@ -1,27 +1,55 @@
-from typing import List
+from typing import List, Union, Set
 
 from domain.collections import Range
 from domain.events import Event
 from domain.ids import PrefixedUUID
-from domain.persistence.repositories import LocationRepository, EventRepository
+from domain.persistence.repositories import LocationRepository, EventRepository, TravelerRepository
+from domain.positions import Position, MovementType
 
 
 class TimelineUseCase:
     _location_repository: LocationRepository
+    _traveler_repository: TravelerRepository
     _event_repository: EventRepository
 
-    def __init__(self, location_repository: LocationRepository, event_repository: EventRepository) -> None:
+    def __init__(self, location_repository: LocationRepository, traveler_repository: TravelerRepository, event_repository: EventRepository) -> None:
         self._location_repository = location_repository
+        self._traveler_repository = traveler_repository
         self._event_repository = event_repository
 
     def construct_location_timeline(self, location_id: PrefixedUUID) -> List[PrefixedUUID]:
-        self._location_repository.retrieve(location_id)
-
-        events = self._event_repository.retrieve_all(location_id=location_id)
-
         def get_continuum(e: Event) -> Range:
             return e.span.continuum
 
-        events_ordered_by_continuum = sorted(events, key=get_continuum)
+        self._location_repository.retrieve(location_id)
+        events = self._event_repository.retrieve_all(location_id=location_id)
 
+        events_ordered_by_continuum = sorted(events, key=get_continuum)
         return [event.id for event in events_ordered_by_continuum]
+
+    def construct_traveler_timeline(self, traveler_id: PrefixedUUID) -> List[Union[PrefixedUUID, Position]]:
+        traveler = self._traveler_repository.retrieve(traveler_id)
+        events = self._event_repository.retrieve_all(traveler_id=traveler_id)
+
+        already_applicable_events: Set[Event] = set([])
+        timeline: List[Union[PrefixedUUID, Position]] = []
+        for positional_move in traveler.journey:
+            curr_position = positional_move.position
+
+            already_applicable_events = {event for event in already_applicable_events if event.span.includes(curr_position)}
+            newly_applicable_events: Set[Event] = {
+                event for event in events
+                if event.span.includes(curr_position) and event not in already_applicable_events
+            }
+            already_applicable_events.update(newly_applicable_events)
+
+            if positional_move.movement_type == MovementType.IMMEDIATE:
+                timeline.append(curr_position)
+                timeline.extend(map(lambda event: event.id, newly_applicable_events))
+            else:
+                timeline.extend(map(lambda event: event.id, newly_applicable_events))
+                timeline.append(curr_position)
+
+        return timeline
+
+
--- Test/Unittest/application/test_timeline_use_cases.py
+++ Test/Unittest/application/test_timeline_use_cases.py
@@ -2,11 +2,12 @@ from math import inf
 from random import shuffle
 from unittest import TestCase
 
-from Test.Unittest.test_helpers.anons import anon_prefixed_id, anon_event, anon_positional_range, anon_location
+from Test.Unittest.test_helpers.anons import anon_prefixed_id, anon_event, anon_positional_range, anon_location, anon_traveler
 from adapter.persistence.in_memory_repositories import InMemoryLocationRepository, InMemoryTravelerRepository, InMemoryEventRepository
 from application.timeline_use_cases import TimelineUseCase
 from domain.collections import Range
 from domain.persistence.repositories import LocationRepository, TravelerRepository, EventRepository
+from domain.positions import Position, PositionalMove, MovementType, PositionalRange
 
 
 class TestTimelineUseCase(TestCase):
@@ -19,7 +20,8 @@ class TestTimelineUseCase(TestCase):
         self.location_repository = InMemoryLocationRepository()
         self.traveler_repository = InMemoryTravelerRepository()
         self.event_repository = InMemoryEventRepository()
-        self.timeline_use_case = TimelineUseCase(self.location_repository, self.event_repository)
+        self.timeline_use_case = TimelineUseCase(self.location_repository, self.traveler_repository, self.event_repository)
+        self.maxDiff = None
 
     def test__construct_location_timeline__should_reject_nonexistent_location(self) -> None:
         # Arrange
@@ -27,7 +29,7 @@ class TestTimelineUseCase(TestCase):
 
         # Act
         def action(): self.timeline_use_case.construct_location_timeline(location_id)
-        
+
         # Assert
         self.assertRaises(NameError, action)
 
@@ -57,6 +59,157 @@ class TestTimelineUseCase(TestCase):
 
         # Act
         actual = self.timeline_use_case.construct_location_timeline(location_id)
+
+        # Assert
+        self.assertListEqual(expected_timeline, actual)
+
+    def test__construct_traveler_timeline__should_reject_nonexistent_traveler(self) -> None:
+        # Arrange
+        traveler_id = anon_prefixed_id(prefix="traveler")
+
+        # Act
+        def action(): self.timeline_use_case.construct_traveler_timeline(traveler_id)
+
+        # Assert
+        self.assertRaises(NameError, action)
+
+    def test__construct_traveler_timeline__should_return_same_event_multiple_times_when_travelers_journey_intersects_it_multiple_times(self) -> None:
+        # Arrange
+        initially_on_ground = Position(latitude=1, longitude=1, altitude=0, continuum=1, reality=0)
+        jumping_upward = Position(latitude=1, longitude=1, altitude=5, continuum=2, reality=0)
+        jump_peak = Position(latitude=1, longitude=1, altitude=10, continuum=3, reality=0)
+        falling_downward = Position(latitude=1, longitude=1, altitude=5, continuum=4, reality=0)
+        back_on_ground = Position(latitude=1, longitude=1, altitude=0, continuum=5, reality=0)
+
+        near_ground_span = PositionalRange(latitude=Range(-2, 2), longitude=Range(-2, 2), altitude=Range(-2, 2), continuum=Range(0, 5),
+                                           reality=Range(0, 0))
+        in_air_span = PositionalRange(latitude=Range(-2, 2), longitude=Range(-2, 2), altitude=Range(8, 12), continuum=Range(0, 5),
+                                      reality=Range(0, 0))
+
+        traveler = anon_traveler(journey=[
+            PositionalMove(position=initially_on_ground, movement_type=MovementType.IMMEDIATE),
+            PositionalMove(position=jumping_upward, movement_type=MovementType.INTERPOLATED),
+            PositionalMove(position=jump_peak, movement_type=MovementType.INTERPOLATED),
+            PositionalMove(position=falling_downward, movement_type=MovementType.INTERPOLATED),
+            PositionalMove(position=back_on_ground, movement_type=MovementType.INTERPOLATED),
+        ])
+        on_ground_event = anon_event(span=near_ground_span, affected_travelers={traveler.id})
+        in_air_event = anon_event(span=in_air_span, affected_travelers={traveler.id})
+
+        self.traveler_repository.save(traveler)
+        self.event_repository.save(on_ground_event)
+        self.event_repository.save(in_air_event)
+
+        expected_timeline = [
+            initially_on_ground,
+            on_ground_event.id,
+            jumping_upward,
+            in_air_event.id,
+            jump_peak,
+            falling_downward,
+            on_ground_event.id,
+            back_on_ground,
+        ]
+
+        # Act
+        actual = self.timeline_use_case.construct_traveler_timeline(traveler.id)
+
+        # Assert
+        self.assertListEqual(expected_timeline, actual)
+
+    def test__construct_traveler_timeline__should_put_affecting_events_after_the_position__when_movement_type_is_immediate(self) -> None:
+        # Arrange
+        outside_affected_area = Position(latitude=1, longitude=1, altitude=0, continuum=1, reality=0)
+        inside_affected_area = Position(latitude=5, longitude=1, altitude=0, continuum=2, reality=0)
+
+        affected_area = PositionalRange(latitude=Range(4, 6), longitude=Range(1, 1), altitude=Range(0, 0), continuum=Range(0, 3), reality=Range(0, 0))
+
+        traveler = anon_traveler(journey=[
+            PositionalMove(position=outside_affected_area, movement_type=MovementType.IMMEDIATE),
+            PositionalMove(position=inside_affected_area, movement_type=MovementType.IMMEDIATE),
+        ])
+        event = anon_event(span=affected_area, affected_travelers={traveler.id})
+
+        self.traveler_repository.save(traveler)
+        self.event_repository.save(event)
+
+        expected_timeline = [
+            outside_affected_area,
+            inside_affected_area,
+            event.id,
+        ]
+
+        # Act
+        actual = self.timeline_use_case.construct_traveler_timeline(traveler.id)
+
+        # Assert
+        self.assertListEqual(expected_timeline, actual)
+
+    def test__construct_traveler_timeline__should_put_affecting_events_before_the_position__when_movement_type_is_interpolated(self) -> None:
+        # Arrange
+        outside_affected_area = Position(latitude=1, longitude=1, altitude=0, continuum=1, reality=0)
+        inside_affected_area = Position(latitude=5, longitude=1, altitude=0, continuum=2, reality=0)
+
+        affected_area = PositionalRange(latitude=Range(4, 6), longitude=Range(1, 1), altitude=Range(0, 0), continuum=Range(0, 3), reality=Range(0, 0))
+
+        traveler = anon_traveler(journey=[
+            PositionalMove(position=outside_affected_area, movement_type=MovementType.IMMEDIATE),
+            PositionalMove(position=inside_affected_area, movement_type=MovementType.INTERPOLATED),
+        ])
+        event = anon_event(span=affected_area, affected_travelers={traveler.id})
+
+        self.traveler_repository.save(traveler)
+        self.event_repository.save(event)
+
+        expected_timeline = [
+            outside_affected_area,
+            event.id,
+            inside_affected_area,
+        ]
+
+        # Act
+        actual = self.timeline_use_case.construct_traveler_timeline(traveler.id)
+
+        # Assert
+        self.assertListEqual(expected_timeline, actual)
+
+    def test__construct_traveler_timeline__should_not_report_same_event_multiple_times__when_traveler_does_not_leave_events_span(self) -> None:
+        # Arrange
+        pos_1_outside_affected_area = Position(latitude=1, longitude=1, altitude=0, continuum=1, reality=0)
+        pos_2_inside_affected_area_a = Position(latitude=5, longitude=1, altitude=0, continuum=2, reality=0)
+        pos_3_inside_affected_area_b = Position(latitude=6, longitude=1, altitude=0, continuum=3, reality=0)
+        pos_4_inside_affected_area_a = Position(latitude=5, longitude=1, altitude=0, continuum=4, reality=0)
+        pos_5_outside_affected_area = Position(latitude=1, longitude=1, altitude=0, continuum=5, reality=0)
+        pos_6_inside_affected_area_b = Position(latitude=6, longitude=1, altitude=0, continuum=6, reality=0)
+
+        affected_area = PositionalRange(latitude=Range(4, 6), longitude=Range(1, 1), altitude=Range(0, 0), continuum=Range(0, 6), reality=Range(0, 0))
+
+        traveler = anon_traveler(journey=[
+            PositionalMove(position=pos_1_outside_affected_area, movement_type=MovementType.IMMEDIATE),
+            PositionalMove(position=pos_2_inside_affected_area_a, movement_type=MovementType.INTERPOLATED),
+            PositionalMove(position=pos_3_inside_affected_area_b, movement_type=MovementType.INTERPOLATED),
+            PositionalMove(position=pos_4_inside_affected_area_a, movement_type=MovementType.IMMEDIATE),
+            PositionalMove(position=pos_5_outside_affected_area, movement_type=MovementType.INTERPOLATED),
+            PositionalMove(position=pos_6_inside_affected_area_b, movement_type=MovementType.INTERPOLATED),
+        ])
+        event = anon_event(span=affected_area, affected_travelers={traveler.id})
+
+        self.traveler_repository.save(traveler)
+        self.event_repository.save(event)
+
+        expected_timeline = [
+            pos_1_outside_affected_area,
+            event.id,
+            pos_2_inside_affected_area_a,
+            pos_3_inside_affected_area_b,
+            pos_4_inside_affected_area_a,
+            pos_5_outside_affected_area,
+            event.id,
+            pos_6_inside_affected_area_b,
+        ]
+
+        # Act
+        actual = self.timeline_use_case.construct_traveler_timeline(traveler.id)
 
         # Assert
         self.assertListEqual(expected_timeline, actual)
