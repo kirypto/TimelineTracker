4c88ab6c85510abd3e8b6928b3820871ecfcc424
Garrett Hansen <kirypto@gmail.com>
2021-11-26 14:42:18 -0600

    Improve requires_authentication to validate arguments and log authentication success (#97)

--- Source/Python/application/access/authentication.py
+++ Source/Python/application/access/authentication.py
@@ -1,6 +1,6 @@
 from functools import wraps
 from typing import Callable
-from logging import warning
+from logging import warning, error, info
 
 from application.access.clients import Profile
 from application.access.errors import AuthError
@@ -12,8 +12,14 @@ def requires_authentication(*, profile_pass_through: bool = False) -> Callable:
         def decorated(*args, profile: Profile = None, **kwargs):
             if profile is None:
                 # TODO #97 kirypto 2021-11-24: Raise the AuthError instead of logging error
-                error = AuthError("Insufficient authentication for this resource.")
-                warning("In the future, authentication will be required for this resource.", exc_info=error)
+                error_message = AuthError("Insufficient authentication for this resource.")
+                warning("In the future, authentication will be required for this resource.", exc_info=error_message)
+            elif not isinstance(profile, Profile):
+                error(f"Argument profile is of invalid type, was {type(profile)}")
+                raise TypeError(f"Argument profile is of invalid type.")
+            else:
+                info(f"Authenticated {profile.name} for access to {function.__qualname__}")
+
             if profile_pass_through:
                 kwargs["profile"] = profile
             return function(*args, **kwargs)
