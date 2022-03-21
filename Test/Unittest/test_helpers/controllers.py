3f53d6de54ad391556c8f7fc08bb831e3a849213
Garrett Hansen <kirypto@gmail.com>
2022-03-20 19:56:27 -0600

    Add support for Profile in TestableRESTController (#96)

--- Test/Unittest/test_helpers/controllers.py
+++ Test/Unittest/test_helpers/controllers.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
+from application.access.clients import Profile
 from application.requests.rest import RESTMethod, MIMEType, RequestHandler, HandlerResult
 from application.requests.rest.controllers import RESTController, HandlerRegisterer, validate_route_handler_declaration
 from application.requests.rest.utils import with_error_response_on_raised_exceptions
@@ -8,9 +9,11 @@ from application.requests.rest.utils import with_error_response_on_raised_except
 
 class TestableRESTController(RESTController):
     _handlers: Dict[str, Dict[RESTMethod, RequestHandler]]
+    profile: Optional[Profile]
 
     def __init__(self):
         self._handlers = defaultdict(dict)
+        self.profile = None
 
     def register_rest_endpoint(self, route: str, method: RESTMethod, response_type: MIMEType = MIMEType.JSON, *, json: bool = False,
                                query_params: bool = False) -> HandlerRegisterer:
@@ -19,6 +22,8 @@ class TestableRESTController(RESTController):
 
             @with_error_response_on_raised_exceptions
             def handler_wrapper(*args, **kwargs) -> HandlerResult:
+                if "profile" not in kwargs and self.profile is not None:
+                    kwargs["profile"] = self.profile
                 return handler_func(*args, **kwargs)
 
             self._handlers[route][method] = handler_wrapper
